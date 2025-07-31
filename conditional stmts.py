02449ca78a477c6accfebc27c248c57074e7203d
Nandhana Madhu <nandhanamadhu931@gmail.com>
2025-07-31 10:35:01 +0530

    conditional statements

---
+++ conditional stmts.py
@@ -0,0 +1,12 @@
+
+"""age=int(input("enter your age:"))
+if age>=18:
+    print("Eligibile for voting")
+else:
+    print("Not eligible for voting")"""
+
+number=int(input ("Enter a number:"))
+if number%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")
